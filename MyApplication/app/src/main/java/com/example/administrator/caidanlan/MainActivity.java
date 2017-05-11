52f6eb34d34b1531a31aae274bde2ba53733c407
michaelleeeee <lizhetongliao@163.com>
2017-05-11 16:14:49 +0800

    配置地图项目

--- MyApplication/app/src/main/java/com/example/administrator/caidanlan/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/caidanlan/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.administrator.caidanlan;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.amap.api.maps.MapView;
 
+public class MainActivity extends AppCompatActivity {
+MapView mapView=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapView=(MapView)findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
     }
 }
