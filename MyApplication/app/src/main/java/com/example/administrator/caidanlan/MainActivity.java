004b0157d6b5bc2b9f5425919c852acaef1d03fd
michaelleeeee <lizhetongliao@163.com>
2017-05-11 22:04:34 +0800

    测试地图能否使用

--- MyApplication/app/src/main/java/com/example/administrator/caidanlan/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/caidanlan/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.administrator.caidanlan;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.amap.api.maps.MapView;
+
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
 
 public class MainActivity extends AppCompatActivity {
 MapView mapView=null;
@@ -13,5 +15,9 @@ MapView mapView=null;
         setContentView(R.layout.activity_main);
         mapView=(MapView)findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
+        AMap aMap=null;
+        if (aMap == null) {
+            aMap = mapView.getMap();
+        }
     }
 }
